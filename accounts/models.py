cf6d02f1a5f501bde974d29b31d09b3094ee6a8b
Malcolm <malcolm.maclure@gmail.com>
2019-04-09 17:34:14 -0400

    models: accounts, recipes

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Accounts(models.Model):
-    name = models.CharField(max-length=120)
-    email = models.CharField(max-length=120)
-    password = models.CharField(max-length=120)
+class Account(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
--- recipes/models.py
+++ recipes/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=120)
-    ingredients = models.TextField(blank=True, null-True)
-    instructions = models.TextField(blank=True, null-True)
+    ingredients = models.TextField(blank=True, null=True)
+    instructions = models.TextField(blank=True, null=True)
