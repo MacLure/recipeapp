e899252baf0f511922c5ee5f8eb662adc5c6ac89
Malcolm <malcolm.maclure@gmail.com>
2019-04-09 20:14:28 -0400

    added base template

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from recipeapp import views
 
 urlpatterns = [
+    path('', views.home_view, name="home"),
     path('admin/', admin.site.urls),
     path('recipes/', include('recipes.urls')),
     path('accounts/', include('accounts.urls')),
