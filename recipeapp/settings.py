0c510c5e030494e0aa9b51bd4b164446549cf99f
Malcolm <malcolm.maclure@gmail.com>
2019-04-13 12:23:38 -0400

    static paths

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -126,3 +126,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from recipeapp import views
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
