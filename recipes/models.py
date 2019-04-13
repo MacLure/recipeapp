6a3328a747b0d4a1f59938caec10ea269cb48e29
Malcolm <malcolm.maclure@gmail.com>
2019-04-13 11:53:43 -0400

    default image

--- recipes/models.py
+++ recipes/models.py
@@ -6,4 +6,4 @@ class Recipe(models.Model):
     name = models.CharField(max_length=120)
     ingredients = models.TextField(blank=True, null=True)
     instructions = models.TextField(blank=True, null=True)
-    image = models.ImageField(default='default.png', blank=True)
+    image = models.ImageField(default='default.jpeg', blank=True)
