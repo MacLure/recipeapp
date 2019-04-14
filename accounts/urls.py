82c3f06efb4e78c0df54b6c787bdcea9ce2463cd
Malcolm <malcolm.maclure@gmail.com>
2019-04-14 12:53:42 -0400

    logout button

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ from . import views
 from .views import (
     account_view,
     signup_view,
+    login_view,
+    logout_view
 )
 
 app_name = 'accounts'
@@ -13,5 +15,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('account/', account_view),
     path('signup/', signup_view, name="signup"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+
 
 ]
